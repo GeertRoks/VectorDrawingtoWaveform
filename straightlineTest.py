7e7e71b3f69c1c5f8800b334ce00f17cdd7b06b2
Geert Roks <geertrocks@gmail.com>
2020-10-24 15:17:51 +0200

    FEAT: read curve forward, backward and backforth
    With a simple straight line the option of how to read through the data
    gives three possible waveforms: rising saw (forward), falling saw
    (backward) and triangle (backforth).
    
    Also add some comments and clean up the code from some stray variables

--- straightlineTest.py
+++ straightlineTest.py
@@ -2,11 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-fs = 48000
-f = 440
-
-t = np.arange(0., 1., f/fs)
-
 def linearBezier(p0, p1, t):
     pFinal = np.zeros(2)
     pFinal[0] = (1-t) * p0[0] + t*p1[0]
@@ -18,51 +13,78 @@ def linearBezierComplex(p0, p1, t):
     pFinal = (1-t) * np.real(p0) + t*np.real(p1) + 1.0j*((1-t) * np.imag(p0) + t*np.imag(p1))
     return pFinal
 
-x = np.zeros(math.floor(fs/f))
 
-for ti in range(np.size(t)-1):
-    x[ti] = linearBezier(np.array([-1,-1]), np.array([1,1]), ti)[0]
+def bezierOscillator(f, fs, seconds, readType = "forward"):
+    '''
 
-def bezierOscillator(f, fs, seconds):
+        Input:
+            f(float):           frequency of the oscillator
+            fs(int):            sampling rate of the oscillator
+            seconds(float):     duration of the synthesized sound in seconds
+            readType(string):   The way how the oscillator reads the vector. Options: forward, backward and backforth
+
+        Output:
+            y_real(numpy.array):    synthesized sound from the horizontal movement over the vector image (x-axis, real)
+            y_imag(numpy.array):    synthesized sound from the vertical movement over the vector image (y-axis, imaginary)
+
+    '''
+    # init parameters
     num_samples = fs * seconds
     step_size = f/fs # step size: 1/(samples per period) = 1/(fs/f) = f/fs
     offset = 0.5
 
-    # setup the step waveform through the bezier curve saw form (add option for triangle form)
-    t = np.resize(np.arange(0., 1., step_size), num_samples)
-    t = np.where(t+offset > 1.0, t-(1-offset), t+offset)
+    # setup the step waveform t through the bezier curve: choice for forward, backward or back-and-forth
+    if (readType == "forward" or readType == "backward"):
+        t = np.arange(0., 1., step_size)
+        offset_index = np.where(t >= offset)[0][0]
+        t = np.resize(t, num_samples + offset_index)
+        t = t[offset_index:]
 
+        if (readType == "backward"):
+            t = np.where(t, 1-t, 1.)
+
+    elif (readType == "backforth"):
+        half_t = np.arange(0.,1., step_size*2)
+        offset_index = np.where(half_t >= offset)[0][0]
+        t = np.resize(np.append(half_t, half_t[::-1]), num_samples + offset_index)
+        t = t[offset_index:]
+
+    else:
+        return 0
+
+    # vector coördinates
     p0 = -1-1j
     p1 = 1+1j
 
+    # get signal
     x = linearBezierComplex(p0, p1, t)
 
-    # extract coordinates
-    x_real = np.real(x) * np.pi
-    x_imag = np.imag(x) * np.pi
+    # extract horizontal and vertical movement
+    x_real = np.real(x)
+    x_imag = np.imag(x)
 
-    # generate sines
+    # Scale amplitude of signals
     A = 0.8
-    y_real = A * np.cos(x_real)
-    y_imag = A * np.sin(x_imag)
+    y_real = A * x_real
+    y_imag = A * x_imag
 
 
     # Plot results
-    #plt.figure(1,(9.5,6))
+    plt.figure(1,(9.5,6))
 
-    #plt.subplot(211)
-    #plt.plot(t, x_real, label="x_real")
-    #plt.plot(t, x_imag, label="x_imag")
-    #plt.autoscale(tight = True)
+    plt.subplot(211)
+    plt.plot(t, x_real, label="x_real")
+    plt.plot(t, x_imag, label="x_imag")
+    plt.autoscale(tight = True)
 
-    #plt.subplot(212)
-    #plt.plot(np.arange(0,num_samples/fs,1/fs), y_real, label="y_real")
-    #plt.plot(np.arange(0,num_samples/fs,1/fs), y_imag, label="y_imag")
-    #plt.autoscale(tight = True)
+    plt.subplot(212)
+    plt.plot(np.arange(0,num_samples/fs,1/fs), y_real, label="y_real")
+    plt.plot(np.arange(0,num_samples/fs,1/fs), y_imag, label="y_imag")
+    plt.autoscale(tight = True)
 
-    #plt.tight_layout()
-    #plt.legend()
-    #plt.show()
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
 
     return (y_real, y_imag)
 
